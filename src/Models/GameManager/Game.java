081c70a3734855b14e0a3b7afd9de5d245e8fda8
Taha Elmi <elmi.taha@gmail.com>
2021-07-18 10:21:10 +0430

    fix: some updates to the Game class

--- src/Models/GameManager/Game.java
+++ src/Models/GameManager/Game.java
@@ -1,5 +1,7 @@
 package Models.GameManager;
-public class Game implements Runnable{
+
+public class Game {
+    private static Game instance;
     private Board board;
     private Player player1;
     private Player player2;
@@ -8,11 +10,11 @@ public class Game implements Runnable{
     private TimeManager timeManager;
     private GameMode gameMode;
 
-    public Game(Board board, Player player1, Player player2, GameMode gameMode) {
-        this.board = board;
+    public Game(Player player1, Player player2, GameMode gameMode) {
         this.player1 = player1;
         this.player2 = player2;
         this.gameMode = gameMode;
+        instance = this;
     }
 
     public void initialize() {}
@@ -20,7 +22,8 @@ public class Game implements Runnable{
     public void finish() {}
 
     public void update() {}
-    @Override
-    public void run() {
+
+    public static Game getInstance() {
+        return instance;
     }
 }
