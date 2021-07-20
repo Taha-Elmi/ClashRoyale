631d70e98842eb139bdbecde2c34c868d6819242
Taha Elmi <elmi.taha@gmail.com>
2021-07-20 22:43:58 +0430

    feat: class DataPackage is created

--- src/Models/GameManager/HumanManager.java
+++ src/Models/GameManager/HumanManager.java
@@ -1,10 +1,26 @@
 package Models.GameManager;
 
+import Main.Config;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.net.Socket;
+
 public class HumanManager implements Manager ,Runnable{
     private Player player;
+    private ObjectInputStream objectInputStream;
+    private ObjectOutputStream objectOutputStream;
 
-    public HumanManager(Player player) {
+    public HumanManager(Player player, Socket socket) {
         this.player = player;
+        try {
+            objectInputStream = new ObjectInputStream(socket.getInputStream());
+            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            Config.unknownInputException();
+        }
     }
 
     @Override
@@ -21,4 +37,12 @@ public class HumanManager implements Manager ,Runnable{
     public void run() {
 
     }
+
+    public void sendData(DataPackage dataPackage) {
+
+    }
+}
+
+class DataPackage implements Serializable {
+
 }
